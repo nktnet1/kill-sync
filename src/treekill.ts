7ec6bfcfcd3eeb0a585ed9b701e32152e5bcfd74
Khiet Tam Nguyen <nktnet999@gmail.com>
2023-10-27 00:15:59 +1100

    removed console.log in distributable

--- src/treekill.ts
+++ src/treekill.ts
@@ -26,7 +26,6 @@ const getAllPids = (): PID_ITEM[] =>
     .split('\n')
     .map((row: string) => {
       const [, pid, ppid] = row.match(/\s*(\d+)\s*(\d+)/) ?? [];
-      console.log([pid, ppid]);
       return {
         pid: Number(pid),
         ppid: Number(ppid)
