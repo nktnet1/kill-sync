dd53bdb44c32aad084e2616db5d3a4cfdfa6e011
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-05-17 16:50:07 +1000

    docs: jsdoc added for isKillError utility function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
+/**
+ * Check if the error contains the 'code' property from usage of
+ * process.kill. This code can contain values such as ESRCH.
+ *
+ * @param err the error object that was caught
+ * @returns true if the error contains the 'code' property
+ */
 export const isKillError = (err: unknown) => {
   return typeof err === 'object' && err !== null && 'code' in err;
 };
